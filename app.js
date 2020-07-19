67da2a43970240dce07293606f4dc9722d5fe078
Niv Yulevitch <niv.yulevitch@gmail.com>
2020-07-19 11:48:03 +0300

    draw to table

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-// Call from Server:
-
 function getFromServer (params) {
     const { url, method = "GET" } = params
     return $.ajax({
@@ -7,15 +5,33 @@ function getFromServer (params) {
         method,
     })
 }
- 
-async function init() {
+
+$(async function() {
+    const container = $('#container');
+
     try {
         const result = await getFromServer({url : "https://randomuser.me/api/?results=50"})
         console.log(result);
-        // const renderedResult = result.result
-        // DrawRenderAPI(renderedResult)
-    } catch (error) {
-        console.log(error);
+        draw(result.results)
+    } catch (err) {
+        console.log(err);
     }
-}
-init()
+
+    function draw(data) {
+        container.empty()
+        const tr = data.map((user) => {
+            return getTR(user)
+        })
+
+        container.append(...tr)
+    }
+    
+    function getTR(user) {
+        const row = $('<tr></tr>');
+        const firstName = $(`<td>${user.name.first}</td>`);
+        const lastName = $(`<td>${user.name.last}</td>`);
+        const gender = $(`<td>${user.gender}</td>`);
+
+        return row.append(firstName, lastName, gender);
+    }
+})
